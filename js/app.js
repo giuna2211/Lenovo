1766c5e3f8286e682ca9f9eb5c092850788c2125
gigi giunashvili <gigigiunashvili@gigis-MacBook-Air.local>
2023-03-26 03:08:07 +0400

    sign in procc

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// function greet(name, lastName) {
+//     console.log(`gamarjoba, ${name} ${lastName}!`)
+// }
+
+// greet(`gigi`, `giunashvili`);
+
+
+// (function support() {
+//     console.log(`slava ukraine`);
+// })();
+
+// const support = function () {
+//     console.log(`gigi giunashvili`);
+// };
+
+// support();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
